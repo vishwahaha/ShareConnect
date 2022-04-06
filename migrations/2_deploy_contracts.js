eb0f3c1b242d094630ea4e85bc91fe4f5d6ff181
Vishwa Prakash <vishwaprakash2003@gmail.com>
2022-04-06 20:33:30 +0530

    Deploy GlobalShare.sol

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,9 @@
 var UserStorage = artifacts.require("./UserStorage.sol");
 var FileShare = artifacts.require("./FileShare.sol");
+var GlobalShare = artifacts.require("./GlobalShare.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(UserStorage);
   deployer.deploy(FileShare);
+  deployer.deploy(GlobalShare);
 };
